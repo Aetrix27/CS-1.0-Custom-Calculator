8cdd395cedee207787e3890652fc4b571363f013
Aetrix27 <davedogg323@gmail.com>
2020-09-03 16:04:17 -0700

    Final Quadratic Calculator
    The user is first prompted to enter 3 valid coefficients. Created a function that takes 3 numbers from the user (a trinomial) as input, and calculates a positive and negative root using the quadratic formula. The function then returns a string.

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import math
+
+def calculate_quadratic(coeff_1, coeff_2, coeff_3):
+    #The formula to calculate the quadratic equation for the positive result given by the square
+    #root is found below, the inputted into the appropriate variable.
+    positive_root=(-coeff_2+math.sqrt((coeff_2**2)-(4*(coeff_1*coeff_3))))/(2*coeff_1)
+
+    #The formula to calculate the negative root of the quadratic is used to find the second input variable.
+    negative_root=(-coeff_2-math.sqrt((coeff_2**2)-(4*(coeff_1*coeff_3))))/(2*coeff_1)
+    return f"The positive root is {positive_root} and the negative root is {negative_root}"
+
 #User is prompted to enter three coefficients that will be used in calculating the roots, using
 #the quadratic equation.
+print("INPUT OF 3 NUMBER (trinomial) MUST NOT PRODUCE MAKE NEG SQUARE ROOT OR 0 DENOMINATOR")
+print("EXAMPLES OF VALID TRINOMIALS TO USE: 2, -9, and 10; 9, 12, and 4; and 2, 7, and -4")
+coefficient_1=int(input("Enter the first coefficient. "))
+coefficient_2=int(input("Enter the second coefficient. "))
+coefficient_3=int(input("Enter the third coefficient. "))
 
-coefficient_1=int(input("Enter the first coefficient."))
-coefficient_2=int(input("Enter the second coefficient."))
-coefficient_3=int(input("Enter the third coefficient."))
+#Calls the function to calculate the roots.
+print(calculate_quadratic(coefficient_1,coefficient_2,coefficient_3))
