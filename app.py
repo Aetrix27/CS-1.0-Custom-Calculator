3c0df349fdd00f3ce1f9d88a0c4c7c9d77b89bfd
Aetrix27 <davedogg323@gmail.com>
2020-09-03 14:56:53 -0700

    Create app.py
    Updated the user input system, so that it asks the user for three coefficients, which will be used to calculate a negative and positive root using the quadratic formula.

---
+++ app.py
@@ -0,0 +1,6 @@
+#User is prompted to enter three coefficients that will be used in calculating the roots, using
+#the quadratic equation.
+
+coefficient_1=int(input("Enter the first coefficient."))
+coefficient_2=int(input("Enter the second coefficient."))
+coefficient_3=int(input("Enter the third coefficient."))
